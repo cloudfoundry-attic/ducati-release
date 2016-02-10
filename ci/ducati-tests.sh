748a0729ee541ae1a1c00c9015baf03537e5d614
Matthew Sykes <matthew.sykes@gmail.com>
2016-02-10 14:47:50 -0800

    tests script now runs daemon tests
    [#112863887]
    
    Signed-off-by: Gabe Rosenhouse <grosenhouse@pivotal.io>

--- ci/ducati-tests.sh
+++ ci/ducati-tests.sh
@@ -6,14 +6,14 @@ cd ducati-release
 export GOPATH=$PWD
 
 declare -a packages=(
+  "src/github.com/cloudfoundry-incubator/ducati-daemon"
   "src/github.com/cloudfoundry-incubator/guardian-cni-adapter"
   "src/github.com/cloudfoundry-incubator/ducati-cni-plugins"
   "src/integration"
   "src/github.com/cloudfoundry-incubator/ducati"
   )
 
-for dir in "${packages[@]}"
-do
+for dir in "${packages[@]}"; do
   pushd $dir
     ginkgo -r -failFast -randomizeAllSpecs -randomizeSuites "$@"
   popd
