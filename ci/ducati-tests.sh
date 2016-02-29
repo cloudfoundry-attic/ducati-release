f6300cc973af1a65105857818971133b460664af
David Bellotti <dbellotti@pivotal.io>
2016-02-29 10:22:07 -0800

    Remove legacy "integration" test
    Signed-off-by: Gabe Rosenhouse <grosenhouse@pivotal.io>

--- ci/ducati-tests.sh
+++ ci/ducati-tests.sh
@@ -7,9 +7,8 @@ export GOPATH=$PWD
 
 declare -a packages=(
   "src/github.com/cloudfoundry-incubator/ducati-daemon"
-  "src/github.com/cloudfoundry-incubator/guardian-cni-adapter"
   "src/github.com/cloudfoundry-incubator/ducati-cni-plugins"
-  "src/integration"
+  "src/github.com/cloudfoundry-incubator/guardian-cni-adapter"
   )
 
 function bootPostgres {
