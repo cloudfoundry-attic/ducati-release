b3305510a247d3935a3d61788ff031f5fef8574e
David Bellotti <dbellotti@gmail.com>
2016-03-18 16:02:51 -0400

    Containers can talk to each other
    [##115183737]
    
    bump ducati-daemon
    
    Submodule src/github.com/cloudfoundry-incubator/ducati-daemon 9f5113d..d61b0db:
      > Stop adding vxlan route & restore NUD_PERMANENT
      > Use NUD_REACHABLE and fix logging
      > Use NUD_PERMANENT
      > Add forwarding database entries on ARP miss
      > Remove AddNeigh from Netlinker
      > Add l2 forwarding database entry
    
    Signed-off-by: Matthew Sykes <matthew.sykes@gmail.com>

--- src/acceptance/acceptance_test.go
+++ src/acceptance/acceptance_test.go
@@ -2,6 +2,7 @@ package acceptance_test
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -17,26 +18,44 @@ import (
 )
 
 var _ = Describe("Guardian integration with Ducati", func() {
-	Describe("container creation", func() {
-		var gardenClient1 garden_client.Client
-		var container garden.Container
-		var networkName string
+	const networkName = "vni-1"
+
+	Context("when there is one garden server", func() {
+		var gardenClient1 garden.Client
+		var ducatiClient1 *ducati_client.DaemonClient
+		var gardenContainer garden.Container
+		var ducatiContainer *models.Container
 
 		BeforeEach(func() {
-			networkName = "test-network"
 			gardenAddress := fmt.Sprintf("%s:7777", gardenServer1)
-
 			gardenClient1 = garden_client.New(connection.New("tcp", gardenAddress))
+			ducatiClient1 = ducati_client.New(fmt.Sprintf("http://%s:4001", gardenServer1), http.DefaultClient)
 
 			var err error
-			container, err = gardenClient1.Create(garden.ContainerSpec{
+			gardenContainer, err = gardenClient1.Create(garden.ContainerSpec{
 				Network: networkName,
 			})
 			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() error {
+				containers, err := ducatiClient1.ListNetworkContainers(networkName)
+				if err != nil {
+					return err
+				}
+
+				for _, c := range containers {
+					if c.ID == gardenContainer.Handle() {
+						ducatiContainer = &c
+						return nil
+					}
+				}
+
+				return errors.New("container not found")
+			}, "5s").Should(Succeed())
 		})
 
 		AfterEach(func() {
-			err := gardenClient1.Destroy(container.Handle())
+			err := gardenClient1.Destroy(gardenContainer.Handle())
 			Expect(err).NotTo(HaveOccurred())
 
 			ducatiClient1 := ducati_client.New(fmt.Sprintf("http://%s:4001", gardenServer1), http.DefaultClient)
@@ -61,7 +80,7 @@ var _ = Describe("Guardian integration with Ducati", func() {
 				Stderr: stderr,
 			}
 
-			process, err := container.Run(ifconfigProcess, ifconfigProcessIO)
+			process, err := gardenContainer.Run(ifconfigProcess, ifconfigProcessIO)
 			Expect(err).NotTo(HaveOccurred())
 			Eventually(process.Wait).Should(Equal(0))
 
@@ -70,64 +89,179 @@ var _ = Describe("Guardian integration with Ducati", func() {
 			Expect(output).To(ContainSubstring("eth1"))
 		})
 
-		It("should define routes to for the overlay", func() {
-			ifconfigProcess := garden.ProcessSpec{
-				Path: "/sbin/ip",
-				Args: []string{"route"},
-				User: "root",
-			}
-
-			stdout := &bytes.Buffer{}
-			stderr := &bytes.Buffer{}
-			ifconfigProcessIO := garden.ProcessIO{
-				Stdin:  &bytes.Buffer{},
-				Stdout: stdout,
-				Stderr: stderr,
-			}
-
-			process, err := container.Run(ifconfigProcess, ifconfigProcessIO)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(process.Wait).Should(Equal(0))
-
-			output := stdout.String()
-
-			Expect(output).To(ContainSubstring("192.168.0.0/16 via 192.168."))
-		})
-
-		Context("when there are two garden servers", func() {
-			var gardenClient2 garden.Client
-			var container2 garden.Container
-
-			var ducatiClient1, ducatiClient2 *ducati_client.DaemonClient
+		Context("when containers share a network", func() {
+			var gardenContainer2 garden.Container
+			var ducatiContainer2 *models.Container
 
 			BeforeEach(func() {
-				gardenAddress2 := fmt.Sprintf("%s:7777", gardenServer2)
-
-				gardenClient2 = garden_client.New(connection.New("tcp", gardenAddress2))
-
-				ducatiClient1 = ducati_client.New(fmt.Sprintf("http://%s:4001", gardenServer1), http.DefaultClient)
-				ducatiClient2 = ducati_client.New(fmt.Sprintf("http://%s:4001", gardenServer2), http.DefaultClient)
-
 				var err error
-				container2, err = gardenClient2.Create(garden.ContainerSpec{
+				gardenContainer2, err = gardenClient1.Create(garden.ContainerSpec{
 					Network: networkName,
 				})
 				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(func() error {
+					containers, err := ducatiClient1.ListNetworkContainers(networkName)
+					if err != nil {
+						return err
+					}
+
+					for _, c := range containers {
+						if c.ID == gardenContainer2.Handle() {
+							ducatiContainer2 = &c
+							return nil
+						}
+					}
+
+					return errors.New("container not found")
+				}, "5s").Should(Succeed())
 			})
+
 			AfterEach(func() {
-				err := gardenClient2.Destroy(container2.Handle())
+				err := gardenClient1.Destroy(gardenContainer2.Handle())
 				Expect(err).NotTo(HaveOccurred())
 			})
 
-			It("should share container metadata across the deployment", func() {
-				containersList1, err := ducatiClient1.ListNetworkContainers(networkName)
-				Expect(err).NotTo(HaveOccurred())
+			It("connects the containers", func() {
+				By("pinging from container 1 to container 2")
+				pingContainer2 := garden.ProcessSpec{
+					Path: "/bin/ping",
+					Args: []string{"-c3", ducatiContainer2.IP},
+					User: "root",
+				}
 
-				containersList2, err := ducatiClient2.ListNetworkContainers(networkName)
+				process, err := gardenContainer.Run(pingContainer2, garden.ProcessIO{})
 				Expect(err).NotTo(HaveOccurred())
+				Eventually(process.Wait).Should(Equal(0))
 
-				Expect(containersList1).To(ConsistOf(containersList2))
+				By("pinging from container 2 to container 1")
+				pingContainer1 := garden.ProcessSpec{
+					Path: "/bin/ping",
+					Args: []string{"-c3", ducatiContainer.IP},
+					User: "root",
+				}
+
+				process, err = gardenContainer2.Run(pingContainer1, garden.ProcessIO{})
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(process.Wait).Should(Equal(0))
 			})
+		})
+	})
+
+	Context("when there are two garden servers", func() {
+		var (
+			gardenClient1 garden.Client
+			gardenClient2 garden.Client
+
+			ducatiClient1 *ducati_client.DaemonClient
+			ducatiClient2 *ducati_client.DaemonClient
+
+			gardenContainer  garden.Container
+			gardenContainer2 garden.Container
+
+			ducatiContainer  *models.Container
+			ducatiContainer2 *models.Container
+		)
+
+		BeforeEach(func() {
+			gardenAddress1 := fmt.Sprintf("%s:7777", gardenServer1)
+			gardenAddress2 := fmt.Sprintf("%s:7777", gardenServer2)
+
+			gardenClient1 = garden_client.New(connection.New("tcp", gardenAddress1))
+			gardenClient2 = garden_client.New(connection.New("tcp", gardenAddress2))
+
+			ducatiClient1 = ducati_client.New(fmt.Sprintf("http://%s:4001", gardenServer1), http.DefaultClient)
+			ducatiClient2 = ducati_client.New(fmt.Sprintf("http://%s:4001", gardenServer2), http.DefaultClient)
+
+			var err error
+			gardenContainer, err = gardenClient1.Create(garden.ContainerSpec{
+				Network: networkName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() error {
+				containers, err := ducatiClient1.ListNetworkContainers(networkName)
+				if err != nil {
+					return err
+				}
+
+				for _, c := range containers {
+					if c.ID == gardenContainer.Handle() {
+						ducatiContainer = &c
+						return nil
+					}
+				}
+
+				return errors.New("container not found")
+			}, "5s").Should(Succeed())
+
+			gardenContainer2, err = gardenClient2.Create(garden.ContainerSpec{
+				Network: networkName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() error {
+				containers, err := ducatiClient1.ListNetworkContainers(networkName)
+				if err != nil {
+					return err
+				}
+
+				for _, c := range containers {
+					if c.ID == gardenContainer2.Handle() {
+						ducatiContainer2 = &c
+						return nil
+					}
+				}
+
+				return errors.New("container not found")
+			}, "5s").Should(Succeed())
+		})
+
+		AfterEach(func() {
+			err := gardenClient1.Destroy(gardenContainer.Handle())
+			Expect(err).NotTo(HaveOccurred())
+
+			err = gardenClient2.Destroy(gardenContainer2.Handle())
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() ([]models.Container, error) {
+				containers, err := ducatiClient1.ListNetworkContainers(networkName)
+				return containers, err
+			}, "5s").Should(BeEmpty())
+		})
+
+		It("should share container metadata across the deployment", func() {
+			containersList1, err := ducatiClient1.ListNetworkContainers(networkName)
+			Expect(err).NotTo(HaveOccurred())
+
+			containersList2, err := ducatiClient2.ListNetworkContainers(networkName)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(containersList1).To(ConsistOf(containersList2))
+		})
+
+		It("connects the containers", func() {
+			By("pinging from container 1 to container 2")
+			pingContainer2 := garden.ProcessSpec{
+				Path: "/bin/ping",
+				Args: []string{"-c3", ducatiContainer2.IP},
+				User: "root",
+			}
+
+			process, err := gardenContainer.Run(pingContainer2, garden.ProcessIO{})
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(process.Wait).Should(Equal(0))
+
+			By("pinging from container 2 to container 1")
+			pingContainer1 := garden.ProcessSpec{
+				Path: "/bin/ping",
+				Args: []string{"-c3", ducatiContainer.IP},
+				User: "root",
+			}
+
+			process, err = gardenContainer2.Run(pingContainer1, garden.ProcessIO{})
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(process.Wait).Should(Equal(0))
 		})
 	})
 })
