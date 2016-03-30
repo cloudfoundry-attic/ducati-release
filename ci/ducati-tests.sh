ce46a40519be98476db35195f50ce91f6e74b87b
Matthew Sykes <matthew.sykes@gmail.com>
2016-03-30 11:33:48 -0400

    Add ducti-dns to ci
    [#116135027]
    
    bump ducati-dns dns
    
    Submodule src/github.com/cloudfoundry-incubator/ducati-dns 0000000...6477379 (new submodule)
    Submodule src/github.com/miekg/dns 0000000...7e024ce (new submodule)

--- ci/ducati-tests.sh
+++ ci/ducati-tests.sh
@@ -7,6 +7,7 @@ export GOPATH=$PWD
 
 declare -a packages=(
   "src/github.com/cloudfoundry-incubator/ducati-daemon"
+  "src/github.com/cloudfoundry-incubator/ducati-dns"
   "src/github.com/cloudfoundry-incubator/ducati-cni-plugins"
   "src/github.com/cloudfoundry-incubator/guardian-cni-adapter"
   )
