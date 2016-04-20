be136f9c59f9b3fc038bae397b2f24e9dfbc79a1
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-04-20 16:36:49 -0700

    Adds acceptance test for apps connecting to backend via proxy app
    [#117744593]
    
    Signed-off-by: David Bellotti <dbellotti@pivotal.io>

--- src/acceptance-with-cf/acceptance_with_cf_test.go
+++ src/acceptance-with-cf/acceptance_with_cf_test.go
@@ -15,21 +15,45 @@ const Timeout_Push = 1 * time.Minute
 const Timeout_Short = 10 * time.Second
 
 var _ = Describe("Ducati CF acceptance tests", func() {
-	var appName string
+	var proxyApp string
+	var backendApp string
 
 	BeforeEach(func() {
-		appName = generator.PrefixedRandomName("ducati-test-app-")
+		proxyApp = generator.PrefixedRandomName("ducati-test-proxy-app-")
+		backendApp = generator.PrefixedRandomName("ducati-test-backend-app-")
 
-		Expect(cf.Cf("push", appName, "-p", "example-apps/proxy", "-f", "example-apps/proxy/manifest.yml").Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("push", proxyApp, "-p", "example-apps/proxy", "-f", "example-apps/proxy/manifest.yml").Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("push", backendApp, "-p", "example-apps/proxy", "-f", "example-apps/proxy/manifest.yml").Wait(Timeout_Push)).To(gexec.Exit(0))
 	})
 
 	AfterEach(func() {
-		Expect(cf.Cf("delete", appName, "-f", "-r").Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("delete", proxyApp, "-f", "-r").Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("delete", backendApp, "-f", "-r").Wait(Timeout_Push)).To(gexec.Exit(0))
 	})
 
-	It("makes the app reachachable", func() {
+	It("makes everything reachable", func() {
+
+		By("checking that the proxy is reachable via its external route")
 		Eventually(func() string {
-			return helpers.CurlAppRoot(appName)
+			return helpers.CurlApp(proxyApp, "/")
 		}, Timeout_Short).Should(ContainSubstring("hello, this is proxy"))
+
+		By("checking that the backend is reachable via its external route")
+		Eventually(func() string {
+			return helpers.CurlApp(backendApp, "/")
+		}, Timeout_Short).Should(ContainSubstring("hello, this is proxy"))
+
+		By("checking that the backend is reachable via the proxy at its **external** route")
+		backendWithoutScheme := backendApp + "." + helpers.LoadConfig().AppsDomain
+		Eventually(func() string {
+			return helpers.CurlApp(proxyApp, "/proxy/"+backendWithoutScheme)
+		}, Timeout_Short).Should(ContainSubstring("hello, this is proxy"))
+
+		// TODO: uncomment when internal DNS works
+		// By("checking that the backend is reachable via the proxy at its **internal** route")
+		// backendWithoutScheme = "some.internal.route.to.backend.potato"
+		// Eventually(func() string {
+		// 	return helpers.CurlApp(proxyApp, "/proxy/"+backendWithoutScheme)
+		// }, Timeout_Short).Should(ContainSubstring("hello, this is proxy"))
 	})
 })
