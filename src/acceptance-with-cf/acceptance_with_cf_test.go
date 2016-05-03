ed05aab5e43f5e3980048cb3501c71b5f1744497
David Bellotti <dbellotti@pivotal.io>
2016-05-03 12:39:27 -0700

    Acceptance errand confirms space isolation
     - Set up ducati-acceptance to run as bosh errand
     - Acceptance with cf w/ binary proxy & and buildpack
     - [#118503437]
    
    Signed-off-by: Jay Dunkelberger <ldunkelberger@pivotal.io>

--- src/acceptance-with-cf/acceptance_with_cf_test.go
+++ src/acceptance-with-cf/acceptance_with_cf_test.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
-	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -16,24 +15,48 @@ const Timeout_Push = 5 * time.Minute
 const Timeout_Short = 10 * time.Second
 
 var _ = Describe("Ducati CF acceptance tests", func() {
-	var proxyApp string
-	var backendApp string
+	var (
+		proxyApp      string
+		backendApp    string
+		backendAppURL string
+		proxyApp2     string
+		orgName       string
+	)
 
 	BeforeEach(func() {
-		proxyApp = generator.PrefixedRandomName("ducati-test-proxy-app-")
-		backendApp = generator.PrefixedRandomName("ducati-test-backend-app-")
+		proxyApp = "proxy-app-1"
+		backendApp = "backend-app"
+		proxyApp2 = "proxy-app-2"
 
-		Expect(cf.Cf("push", proxyApp, "-p", "example-apps/proxy", "-f", "example-apps/proxy/manifest.yml").Wait(Timeout_Push)).To(gexec.Exit(0))
-		Expect(cf.Cf("push", backendApp, "-p", "example-apps/proxy", "-f", "example-apps/proxy/manifest.yml").Wait(Timeout_Push)).To(gexec.Exit(0))
+		orgName = "test-org"
+		Expect(cf.Cf("create-org", orgName).Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("target", "-o", orgName).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+		firstSpace := "space1"
+		Expect(cf.Cf("create-space", firstSpace).Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("target", "-o", orgName, "-s", firstSpace).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+		pushApp(proxyApp)
+		pushApp(backendApp)
+
+		session := cf.Cf("app", backendApp, "--guid")
+		Expect(session.Wait(Timeout_Push)).To(gexec.Exit(0))
+		backendAppGuid := strings.TrimSpace(string(session.Out.Contents()))
+		backendAppURL = backendAppGuid + ".cloudfoundry"
+
+		secondSpace := "space2"
+		Expect(cf.Cf("create-space", secondSpace).Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("target", "-o", orgName, "-s", secondSpace).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+		pushApp(proxyApp2)
 	})
 
 	AfterEach(func() {
-		Expect(cf.Cf("delete", proxyApp, "-f", "-r").Wait(Timeout_Push)).To(gexec.Exit(0))
-		Expect(cf.Cf("delete", backendApp, "-f", "-r").Wait(Timeout_Push)).To(gexec.Exit(0))
+		// clean up everything
+		Expect(cf.Cf("delete-org", orgName, "-f").Wait(Timeout_Push)).To(gexec.Exit(0))
 	})
 
 	It("makes everything reachable", func() {
-
 		By("checking that the proxy is reachable via its external route")
 		Eventually(func() string {
 			return helpers.CurlApp(proxyApp, "/")
@@ -51,12 +74,13 @@ var _ = Describe("Ducati CF acceptance tests", func() {
 		}, Timeout_Short).Should(ContainSubstring("hello, this is proxy"))
 
 		By("checking that the backend is reachable via the proxy at its **internal** route")
-		session := cf.Cf("app", backendApp, "--guid")
-		Expect(session.Wait(Timeout_Push)).To(gexec.Exit(0))
-		appGuid := strings.TrimSpace(string(session.Out.Contents()))
-		appURL := appGuid + ".cloudfoundry"
 		Eventually(func() string {
-			return helpers.CurlApp(proxyApp, "/proxy/"+appURL+":8080")
+			return helpers.CurlApp(proxyApp, "/proxy/"+backendAppURL+":8080")
 		}, Timeout_Short).Should(ContainSubstring("hello, this is proxy"))
+
+		By("checking that the backendApp is NOT reachable from a proxy app in a different space")
+		Eventually(func() string {
+			return helpers.CurlAppWithTimeout(proxyApp2, "/proxy/"+backendAppURL+":8080", 6*Timeout_Short)
+		}, 6*Timeout_Short).Should(ContainSubstring("request failed"))
 	})
 })
--- src/acceptance-with-cf/init_test.go
+++ src/acceptance-with-cf/init_test.go
@@ -1,34 +1,58 @@
 package acceptance_test
 
 import (
+	"os"
+	"path/filepath"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 
 	"testing"
 )
 
 var (
-	context helpers.SuiteContext
-	config  helpers.Config
+	appDir string
+	config helpers.Config
 )
 
 func TestAcceptance(t *testing.T) {
-
 	RegisterFailHandler(Fail)
 
-	config = helpers.LoadConfig()
-
-	context = helpers.NewContext(config)
-	environment := helpers.NewEnvironment(context)
-
 	BeforeSuite(func() {
-		environment.Setup()
+		config = helpers.LoadConfig()
+
+		Expect(cf.Cf("api", "--skip-ssl-validation", config.ApiEndpoint).Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("auth", config.AdminUser, config.AdminPassword).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+		appDir = os.Getenv("APP_DIR")
+		Expect(appDir).NotTo(BeEmpty())
+
+		// create binary
+		os.Setenv("GOOS", "linux")
+		os.Setenv("GOARCH", "amd64")
+		binaryPath, err := gexec.Build(appDir)
+		Expect(err).NotTo(HaveOccurred())
+		err = os.Rename(binaryPath, filepath.Join(appDir, "proxy"))
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	AfterSuite(func() {
-		environment.Teardown()
+		// remove binary
+		err := os.Remove(filepath.Join(appDir, "proxy"))
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	RunSpecs(t, "Acceptance Suite")
+}
+
+func pushApp(appName string) {
+	Expect(cf.Cf(
+		"push", appName,
+		"-p", appDir,
+		"-f", filepath.Join(appDir, "manifest.yml"),
+		"-b", "binary_buildpack",
+	).Wait(Timeout_Push)).To(gexec.Exit(0))
 }
